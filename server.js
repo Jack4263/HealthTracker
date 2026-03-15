6b00711e1aedf8e763a6bb88ca44549e1a140047
Jack4263 <134334519+Jack4263@users.noreply.github.com>
2026-03-15 16:38:08 +0000

    Creating functions for logging in and signing up, hasing passwords and adding to db

--- server.js
+++ server.js
@@ -1,13 +1,51 @@
 const express = require("express");
 const sqlite3 = require("sqlite3").verbose();
-
+const bcrypt = require("bcrypt");
 const app = express();
 const db = new sqlite3.Database("HealthDB.db");
-
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
 // DATABASE CREATION AND FUNCTIONS ------
-db.run('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARRY KEY AUTO INCREMENT, username TEXT, email TEXT, password TEXT)')
+db.run(
+  "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, email TEXT, password TEXT)",
+);
+
+// signup function
+app.post("/signup", async (req, res) => {
+  try {
+    const hashedPass = await bcrypt.hash(password, 10);
+    const query =
+      "INSERT INTO users(username, email, password) VALUES (?, ?, ?)";
+    db.run(query, [username, ElementInternals, hashedPass], function (err) {
+      if (err) {
+        console.error(err);
+        return res.status(500).send("Error creating user.");
+      }
+      res.send("User created successfully");
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server error");
+  }
+});
+//login function
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  db.get("SELECT * FROM users WHERE email = ?", [email], async (err, user) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("Server error");
+    }
+    if (!user) returnres.status(400).send("User not found");
+
+    // hashing input password to stored hashed password
+    const match = await bcrypt.compare(password, user.password);
+    if (match) {
+      res.send("Login successful");
+    }else{
+        res.status(400).send("Incorrect password");
+    }
+  });
+});
