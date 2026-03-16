012b4b245b071abb27b1d01b477da0130630ddca
Jack4263 <134334519+Jack4263@users.noreply.github.com>
2026-03-16 17:18:56 +0000

    Added user exist function, stops users creating an account if it already exists

--- server.js
+++ server.js
@@ -12,11 +12,27 @@ app.set("view engine", "ejs");
 db.run(
   "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, email TEXT, password TEXT)",
 );
+// function to check if user exists already (for signup)
+function userExists(username, email) {
+  return new Promise((resolve, reject) => {
+    const query = "SELECT * FROM users WHERE username = ? or email = ?";
+    db.get(query, [username, email], (err, row) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(!!row);
+      }
+    });
+  });
+}
 
 // signup function
 app.post("/signup", async (req, res) => {
   const { username, email, password } = req.body;
   try {
+    if (await userExists(username, email)) {
+      return res.status(400).send("This user already exists");
+    }
     const hashedPassword = await bcrypt.hash(password, 10);
     const query =
       "INSERT INTO users(username, email, password) VALUES (?, ?, ?)";
@@ -33,25 +49,29 @@ app.post("/signup", async (req, res) => {
     res.status(500).send("Server error");
   }
 });
+
 //login function
 app.post("/login", async (req, res) => {
   const { username, password } = req.body;
-  db.get("SELECT * FROM users WHERE username = ?", [username], async (err, user) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).send("Server error");
-    }
-    if (!user) return res.status(400).send("User not found");
+  db.get(
+    "SELECT * FROM users WHERE username = ?",
+    [username],
+    async (err, user) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send("Server error");
+      }
+      if (!user) return res.status(400).send("User not found");
 
-    // hashing input password to stored hashed password
-    const match = await bcrypt.compare(password, user.password);
-    if (match) {
-      
-      res.render("dashboard", { username: user.username });
-    } else {
-      res.status(400).send("Incorrect password");
-    }
-  });
+      // hashing input password to stored hashed password
+      const match = await bcrypt.compare(password, user.password);
+      if (match) {
+        res.render("dashboard", { username: user.username });
+      } else {
+        res.status(400).send("Incorrect password");
+      }
+    },
+  );
 });
 
 // starting server
